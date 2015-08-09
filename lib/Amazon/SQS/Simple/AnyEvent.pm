0adf52a90fbf310f61ab1cf8d3582f4f4b836f07
Jeffrey Ryan Thalhammer <jeff@thaljef.org>
2015-08-09 02:21:39 -0400

    Intitial commit.

---
+++ lib/Amazon/SQS/Simple/AnyEvent.pm
@@ -0,0 +1,277 @@
+package Amazon::SQS::Simple::AnyEvent;
+
+use strict;
+use warnings;
+
+use AnyEvent::HTTP;
+use Amazon::SQS::Simple;
+use Amazon::SQS::Simple::Base;
+use XML::Hash::XS;
+
+#------------------------------------------------------------------------------
+
+our $VERSION = 0.01;
+
+#------------------------------------------------------------------------------
+# Replace methods in Amazon::SQS::Simple with ones that call our non-blocking
+# versions if invoked with a callback. Otherwise, call the original version.
+
+my $class_prefix = "Amazon::SQS::Simple";
+
+my $queue_prefix  = "${class_prefix}::Queue";
+my @queue_actions = qw(Send Receive Delete);
+my @queue_methods = map {("${_}Message", "${_}MessageBatch")} @queue_actions;
+
+my $base_prefix  = "${class_prefix}::Base";
+my @base_methods = qw(_dispatch);
+
+my %methods = ($queue_prefix => \@queue_methods, $base_prefix => \@base_methods);
+
+while (my ($prefix, $methods) = each %methods) {
+    for my $method (@$methods) {
+        no warnings; no strict qw(refs);
+        my $full_name = $prefix . "::" . $method;
+        my $original  = *{$full_name}{CODE};
+    
+        *{$full_name} = sub {
+            my $has_cb = ref $_[-1] eq "CODE";
+            pop if not defined $_[-1]; # Has undefined cb
+            return $has_cb ? $method->(@_) : $original->(@_);
+        };
+    }
+}
+
+#------------------------------------------------------------------------------
+
+sub ReceiveMessageBatch {
+    my $self   = shift;
+    my $cb     = pop;
+    my %params = @_;
+    
+    $params{MaxNumberOfMessages} = 10;
+
+    return $self->ReceiveMessage(%params, $cb);
+}
+
+#------------------------------------------------------------------------------
+
+sub SendMessage {
+    my $self       = shift;
+    my $message    = shift;
+    my $cb         = pop;
+    my %params     = @_;
+
+    $params{Action} = 'SendMessage';
+    $params{MessageBody} = $message;
+    
+    my $on_response = sub {
+        my $href = shift;
+        my $response = Amazon::SQS::Simple::SendResponse->new($href->{SendMessageResult}, $message);
+        $cb->($response);
+    };
+    
+    return $self->_dispatch(\%params, $on_response);
+}
+
+#------------------------------------------------------------------------------
+
+sub ReceiveMessage {
+    my $self   = shift;
+    my $cb     = pop;
+    my %params = @_;
+    
+    $params{Action} = 'ReceiveMessage';
+
+    my $on_response = sub {
+        my $href = shift;
+
+        my @messages;
+        no strict "refs";
+        if (my $msgs = $href->{ReceiveMessageResult}{Message}) {       
+	    my $api_version   = $self->_api_version;
+	    my $class = "Amazon::SQS::Simple::Message";
+	    push @messages, map { $class->new($_, $api_version) } @$msgs;
+	}
+
+        $cb->(@messages);
+    };
+    
+    return $self->_dispatch(\%params, [qw(Message)], $on_response);
+
+}
+
+#------------------------------------------------------------------------------
+
+sub DeleteMessageBatch {
+    my $self     = shift;
+    my $messages = shift;
+    my $cb       = pop;
+    my %params   = @_;
+
+    return unless @$messages;
+    $params{Action} = 'DeleteMessageBatch';
+
+    my $i=0;
+    foreach my $msg (@$messages){
+        if (++$i > 10){
+            warn "Batch deletion limited to 10 messages";
+            last;
+        }
+
+        $params{"DeleteMessageBatchRequestEntry.$i.Id"} = $msg->MessageId;
+        $params{"DeleteMessageBatchRequestEntry.$i.ReceiptHandle"} = $msg->ReceiptHandle;
+    }
+
+    return $self->_dispatch(\%params, $cb);
+}
+
+#------------------------------------------------------------------------------
+
+sub _dispatch {
+    my $self        = shift;
+    my $cb          = pop;
+    my $params      = shift || {};
+    my $force_array = shift || [];
+
+    my $url = $self->{Endpoint};
+    my $post_request = 0;
+
+    $params = {
+        AWSAccessKeyId      => $self->{AWSAccessKeyId},
+        Version             => $self->{Version},
+        %$params,
+    };
+
+    if (!$params->{Timestamp} && !$params->{Expires}) {
+        $params->{Timestamp} = Amazon::SQS::Simple::Base::_timestamp();
+    }
+    
+    if ($params->{MessageBody} && length($params->{MessageBody}) > $self->_max_get_msg_size) {
+        $post_request = 1;
+    }
+
+    my ($query, @auth_headers) = $self->_get_signed_query($params, $post_request);
+    my %http_opts = (tls_ctx => "low");
+
+    $self->_debug_log($query);
+
+    my $on_response = sub {
+        my ($content, $headers) = @_;
+        $self->_debug_log($content);
+        if ($headers->{Status} =~ /^2/) {
+            my $href = xml2hash($content, force_array => $force_array);
+            $cb->($href);
+        }
+        else {
+            warn "ERROR: $headers->{Status} $headers->{Reason}";
+            $cb->();
+        }
+    };
+
+    if ($post_request) {
+        my $headers = {
+            'Content-Type' => 'application/x-www-form-urlencoded;charset=utf-8',
+            'Content'      => $query,
+            @auth_headers,
+        };
+
+        http_post($url, headers => $headers, %http_opts, $on_response);
+    }
+    else {
+        my $headers = {
+            "Content-Type" => "text/plain;charset=utf-8",
+            @auth_headers,
+        };
+
+        http_get("$url/?$query", headers => $headers, %http_opts, $on_response);
+    }
+}
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Amazon::SQS::Simple::AnyEvent - A non-blocking API to Amazon's SQS
+
+=head1 SYNOPSIS
+
+use Amazon::SQS::Simple;
+use Amazon::SQS::Simple::AnyEvent;
+
+my $sqs = Amazon::SQS::Simple->new($access_key, $secret_key);
+my $queue = $sqs->GetQueue($endpoint);
+
+my $callback = sub {
+  my $message = shift;
+  # do something with message...
+};
+
+my $msg = $queue->ReceiveMessage();       # Blocking
+my $guard = $queue->ReceiveMessage($cb);  # Non-blocking
+
+# do something else...
+
+=head1 DESCRIPTION
+
+This module is a mixin that adds non-blocking capbilities to
+L<Amazon::SQS::Simple> via L<AnyEvent>.
+
+=head1 METHODS
+
+The following methods on L<Amazon::SQS::Simple::Queue> are enhanced
+with non-blocking capabiliites. In all cases, adding a subroutine
+reference as the last argument will cause the method to be called in
+non-blocking mode. If the request is successful, your callback will
+receive the message body as the only argument. The method itself will
+return an C<AnyEvent::Util::guard> object.
+
+Otherwise, the calling interfaces are exactly the same as those
+described in L<Amazon::SQS::Simple::Queue>. In fact, if you do not
+pass a callback argument, then the call is sent straight to the
+original blocking method in L<Amazon::SQS::Simple>. Any method
+not listed here does not have non-blocking capabilities.
+
+=over 4
+
+=item SendMessage($message, [%opts], [sub{...}])
+
+=item SendMessageBatch($messages, [%opts], [sub{...}])
+
+=item ReceiveMessage([%opts], [sub{...}])
+
+=item ReceiveMessageBatch([%opts], [sub{...}])
+
+=item DeleteMessage($message, [%opts], [sub{...}])
+
+=item DeleteMessageBatch($messages, [%opts], [sub{...}])
+
+=back
+
+=head1 AUTHOR
+
+Jeffrey Ryan Thalhammer <jeff@thaljef.org>
+
+=head1 CREDITS
+
+The original authors of L<Amazon::SQS::Simple>:
+
+Simon Whitaker <swhitaker@cpan.org>
+
+Mike Whitaker <penfold@cpan.org>
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2015 by Jeffrey Ryan Thalhammer
+
+=cut
+
+=cut
+
