4eb4494b1ad31b823417236377db64b870f0a23e
Jeffrey Ryan Thalhammer <jeff@thaljef.org>
2015-08-14 15:09:10 -0400

    Simplify example code a bit.

--- examples/parallel.pl
+++ examples/parallel.pl
@@ -7,31 +7,20 @@ use Amazon::SQS::Simple::AnyEvent;
 use Data::Dumper;
 
 #--------------------------------------------------------------------
-# This example shows how to receive 10 message batches in parallel.
-# When each batch is received, the messages will be dumped to STDOUT
-# and then deleted asynchronously.
+# This example fetches 10 message batches in parallel and dumps
+# the messages in each batch to STDOUT when the batch arrives.
 #--------------------------------------------------------------------
 
+# Substitute these arguments with your own...
 my $sqs   = Amazon::SQS::Simple->new("ACCESS_KEY", "SECRET_KEY");
 my $queue = $sqs->GetQueue("QUEUE_NAME");
 
-#--------------------------------------------------------------------
-
 my $cv = AnyEvent->condvar;
+my $cb = sub { print Dumper \@_; $cv->end };
 
 for (1..10) {
-    
     $cv->begin;
-
-    my $cb = sub {
-        my @messages = @_; # Each will be an Amazon::SQS::Simple::Message
-        print Dumper \@messages;
-        $queue->DeleteMessageBatch(\@messages, sub {});
-        $cv->end;
-    };
-
     $queue->ReceiveMessageBatch($cb);
 }
 
-# Blocks until all batches are processed
 $cv->recv;
